22380a3f391aebf5befcba0996e7cd29c6f6fbd2
RoJooHee <rjh123012@naver.com>
2021-05-26 02:56:52 +0900

    지도, 사이드바, 로그인x용화면

--- flask_code/static/assets/dist/js/map.js
+++ flask_code/static/assets/dist/js/map.js
@@ -5,15 +5,47 @@ var mapContainer = document.getElementById('map'), // 지도를 표시할 div
     };
 
 var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+
+// 주소-좌표 변환 객체를 생성합니다
+var geocoder = new kakao.maps.services.Geocoder();
+
+// 주소로 좌표를 검색합니다
+geocoder.addressSearch('서울특별시 중구 퇴계로36길 2', function(result, status) {
+
+    // 정상적으로 검색이 완료됐으면 
+     if (status === kakao.maps.services.Status.OK) {
+
+        var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+        // 결과값으로 받은 위치를 마커로 표시합니다
+        var marker = new kakao.maps.Marker({
+            map: map,
+            position: coords
+        });
+
+        // 인포윈도우로 장소에 대한 설명을 표시합니다
+        var infowindow = new kakao.maps.InfoWindow({
+            content: '<div style="width:150px;text-align:center;padding:6px 0;">동국대학교 충무로영상센터<br>건물<br>서울 중구 퇴계로36길2</div>'
+        });
+        infowindow.open(map, marker);
+
+        // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+        map.setCenter(coords);
+    } 
+});    
+
+
+
 // 마커를 표시할 위치와 내용을 가지고 있는 객체 배열입니다 
 var positions = [
+    //울산 데이터는 바다라서 좌표 기준으로 함
     {
-        content: '<div>충무로_syn</div>', 
-        latlng: new kakao.maps.LatLng(37.560806, 126.993644)
-    },
-    {
-        content: '<div>울산_2214</div>', 
+        content: '<div>울산 현대중공업 <br> 방파제 <br> 임랑 해수욕장 부근 바다</div>',  //마크 위에 표시될 정보내용 (이건 울산_2214 계측기)
         latlng: new kakao.maps.LatLng(35.309242397, 129.267961838)
+    } /*,
+    {
+        content: '<div>충무로 영상센터 </div>', 
+        latlng: new kakao.maps.LatLng(37.560806, 126.993644)
     },
     {
         content: '<div>울산_2221</div>', 
@@ -30,7 +62,7 @@ var positions = [
     {
         content: '<div>울산_2224</div>',
         latlng: new kakao.maps.LatLng(35.309222979, 129.267974469)
-    }
+    } */
 ];
 
 for (var i = 0; i < positions.length; i ++) {
