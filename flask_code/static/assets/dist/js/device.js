85d61a78ae48f2766f3abc04aaf24e2c9ffb5c9c
minshik kim <minshikkim@minshikui-MacBookAir.local>
2021-06-02 05:44:07 +0900

    add weather_info & boxplot rendering

--- flask_code/static/assets/dist/js/device.js
+++ flask_code/static/assets/dist/js/device.js
@@ -1,151 +1,124 @@
-// var lon_a = [ 3733.6460311,
-// 3733.6460293,
-// 3733.6460319,
-// 3733.646036,
-// 3733.6460329,
-// 3733.6460295,
-// 3733.6460298,
-// 3733.6460345,
-// 3733.6460406,
-// 3733.6460257,
-// 3733.6460214,
-// 3733.6460288,
-// 3733.6460206,
-// 3733.6460286,
-// 3733.6460248,
-// 3733.6460293,
-// 3733.6460196,
-// 3733.6460234,
-// 3733.6460153,
-// 3733.646014,
-// 3733.6460225,
-// 3733.6460367,
-// 3733.6460342,
-// 3733.6460268,
-// 3733.6460288,
-// 3733.6460286,
-// 3733.6460292,
-// 3733.6460278,
-// 3733.6460257,
-// 3733.6460374,
-// 3733.6460371,
-// 3733.6460321,
-// 3733.6460258,
-// 3733.646027,
-// 3733.6460337,
-// 3733.6460346,
-// 3733.6460373,
-// 3733.6460265,
-// 3733.6460261,
-// 3733.646027,
-// 3733.6460289,
-// 3733.6460357,
-// 3733.6460436,
-// 3733.6460383,
-// 3733.6460334,
-// 3733.6460343,
-// 3733.6460287,
-// 3733.6460297,
-// 3733.6460254,
-// 3733.6460326
-// ]
-//
-// var lon_b = [
-// 3733.6467077,
-// 3733.646717,
-// 3733.6467061,
-// 3733.6466952,
-// 3733.6466994,
-// 3733.646697,
-// 3733.6466869,
-// 3733.6466608,
-// 3733.6466264,
-// 3733.6466088,
-// 3733.646614,
-// 3733.6466061,
-// 3733.6465729,
-// 3733.6465583,
-// 3733.6465451,
-// 3733.6465178,
-// 3733.6465054,
-// 3733.6464725,
-// 3733.6464387,
-// 3733.6464224,
-// 3733.6463772,
-// 3733.6463486,
-// 3733.6463303,
-// 3733.6463245,
-// 3733.6462939,
-// 3733.6462517,
-// 3733.6462275,
-// 3733.6462224,
-// 3733.6462406,
-// 3733.6462716,
-// 3733.6462804,
-// 3733.6462872,
-// 3733.6462953,
-// 3733.6463019,
-// 3733.6463207,
-// 3733.6463378,
-// 3733.6463366,
-// 3733.6463353,
-// 3733.6463217,
-// 3733.646329,
-// 3733.6463384,
-// 3733.6463373,
-// 3733.6463295,
-// 3733.6463096,
-// 3733.6463099,
-// 3733.6462962,
-// 3733.6462786,
-// 3733.6462745,
-// 3733.6462574,
-// 3733.6462469,
-// 3733.6462478
-// ]
-//
-// var trace1 = {
-//   y: lon_a,
-//   name: '2020-05-23',
-//   type: 'box'
-// };
-//
-// var trace2 = {
-//   y: lon_b,
-//   name: '2020-05-24',
-//   type: 'box'
-// };
-//
-// var data = [trace1, trace2];
-//
-// Plotly.newPlot('box-lan', data);
-//
-// function render_boxplot(){
-//   // create data
-//
-// }
-//
+/**
+ *
+ * @param data(list)
+ * @param elementName
+ */
+function render_plot(data, elementName, title, xAxisName, yAxisName, min, max, absValue){
+    data.reverse();  // 순서변경
 
+    // if(calculateFlag == 1){ // 여기가 동작을 안함.. 0.01을 곱한 결과값으로 바뀌지 않는다..
+    //       // * 10
+    //     for (let i = 0; i <data.length; i++) {
+    //         data[i]['low'] = data[i]['low'] * 0.01
+    //         data[i]['high'] = data[i]['high'] * 0.01
+    //         data[i]['q1'] = data[i]['q1'] * 0.01
+    //         data[i]['q3'] = data[i]['q3'] * 0.01
+    //         data[i]['median'] = data[i]['median'] * 0.01
+    //     }
+    // }
 
-anychart.onDocumentLoad(function () {
-    // render_plot()
-});
-
-function render_plot(data){
-    // var plot = document.getElementById('plot');
-    //create data set on our data
 
     //create area chart
     var chart = anychart.box(data);
+
+    // create a plot on the chart
+
+    // access the annotations() object of the plot to work with annotations
+    var controller = chart.annotations();
+
+    // 측정값 중 최대값
+    var horizontalLineMax = controller.horizontalLine({
+        valueAnchor: max,
+        normal: {stroke: "3 #ff0000"},
+        hovered: {stroke: "3 #ff0000"}
+    });
+
+    // 측정값 중 최소값
+    var horizontalLineMin = controller.horizontalLine({
+        valueAnchor: min,
+        normal: {stroke: "3 #008000"},
+        hovered: {stroke: "3 #008000"},
+    });
+
+    // 기준이 되는 값
+    var horizontalLineAbs = controller.horizontalLine({
+        valueAnchor: absValue,
+        normal: {stroke: "3 #000000"},
+        hovered: {stroke: "3 #000000"},
+    });
+
+    // 최대값 라벨
+    var labelMax = controller.label({
+         xAnchor: data[0]['x'], // 계측 최초 시간
+         valueAnchor: max,
+         fontSize: 15,
+         fontFamily: "Courier",
+         text: "최대" + "\n" + max,
+         normal: {fontColor: "#800000", fontSize: 10},
+         hovered: {fontColor: "#e60000", fontSize: 13},
+         selected: {fontColor: "#e60000", fontSize: 13}
+    });
+
+    // 최소값 라벨
+    var labelMin = controller.label({
+         xAnchor: data[0]['x'], // 계측 최초 시간
+         valueAnchor: min,
+         fontSize: 15,
+         fontFamily: "Courier",
+         text: "최소 : " + min,
+         normal: {fontColor: "#008000", fontSize: 10},
+         hovered: {fontColor: "#00e600", fontSize: 13},
+         selected: {fontColor: "#00e600", fontSize: 13}
+    });
+
+    // 기준값 라벨
+    var labelAbs = controller.label({
+         xAnchor: data[data.length-3]['x'], // 계측 최초 시간
+         valueAnchor: absValue, // 기준값
+         fontSize: 15,
+         fontFamily: "Courier",
+         text: "초기측정값 : " + absValue,
+         normal: {fontColor: "#000000", fontSize: 10},
+         hovered: {fontColor: "#000000", fontSize: 13},
+         selected: {fontColor: "#000000", fontSize: 13}
+    });
+
+
+    chart.title(title)
+
+    // label axis
+    chart.xAxis().title(xAxisName + "\n" + data[0]['x'] + " - " + data[data.length-1]['x']);
+    chart.yAxis().title(yAxisName);
 
     //turn on chart animation
     chart.animation(true);
 
     //set container id for the chart
-    chart.container('plot');
+    chart.container(elementName);
 
     //initiate chart drawing
     chart.draw();
+
+    // // show information when mouse is over a marker
+    // chart.listen("mouseOver", function (e) {
+    //     // var symbol = e.eventMarker.symbol;
+    //     // var description = e.eventMarker.description;
+    //     // var date = e.eventMarker.date;
+    //     console.log(e)
+    //     document.getElementById('information-lat').innerHTML = "heel";
+    //
+    //     // hide information when mouse leaves a marker
+    //     // chart.listen("eventMarkerMouseOut", function () {
+    //     //     document.getElementById(elementName).innerHTML = "";
+    //     // });
+    //     //
+    //     // // open a url when a marker is clicked on
+    //     // chart.listen("eventMarkerClick", function (e) {
+    //     //     var url = "https://www.google.ru/search?q=" +
+    //     //         e.eventMarker.description;
+    //     //     window.open(url, "_blank");
+    //     // });
+    // })
 }
 
 
